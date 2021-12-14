6cf4e2752089b1cbd6f07c908109a479f71a943e
riya40 <priyanunna20@gmail.com>
2021-12-14 22:59:53 +0530

    Added:Employee Presence

--- src/com/bl/EmpWageComputation.java
+++ src/com/bl/EmpWageComputation.java
@@ -5,5 +5,15 @@ public class EmpWageComputation {
 
         //Displaying The Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
+
+        //checking the Employee Presence
+        int rand = (int) Math.floor(Math.random() * 10) % 3;
+
+        if(rand==0){
+            System.out.println("Employee absent");
+        }
+        else {
+            System.out.println("employee present");
+        }
     }
 }
