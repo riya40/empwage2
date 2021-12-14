6385c3638e97cc81d0a5831622a4958c326ac545
riya40 <priyanunna20@gmail.com>
2021-12-14 22:55:45 +0530

    Added:Welcome message

---
+++ src/com/bl/EmpWageComputation.java
@@ -0,0 +1,9 @@
+package com.bl;
+
+public class EmpWageComputation {
+    public static void main(String args[]){
+
+        //Displaying The Welcome Message
+        System.out.println("Welcome To Employee Wage Computation");
+    }
+}
