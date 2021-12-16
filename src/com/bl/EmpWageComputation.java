5bd202ec8f7b573d1d196a5db785fede88f96ccd
riya40 <priyanunna20@gmail.com>
2021-12-16 22:22:59 +0530

    Added:calculating for working days

--- src/com/bl/EmpWageComputation.java
+++ src/com/bl/EmpWageComputation.java
@@ -1,37 +1,41 @@
 package com.bl;
 
 public class EmpWageComputation {
-    public static void main(String args[]){
+    public static final int Working_Days = 20;
+    public static void main(String args[]) {
         int wageperhour = 20, wage = 0;
         int hours = 0, totalhours = 0, total = 0;
 
         //Displaying The Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
 
-        //checking the Employee Presence
-        int rand = (int) Math.floor(Math.random() * 10) % 3;
+        //Calculating the Wage for working days
+        for (int i = 0; i < Working_Days; i++) {
+            //checking the Employee Presence
+            int rand = (int) Math.floor(Math.random() * 10) % 3;
 
-        //Solving with Switch Case Statement
-        switch (rand) {
-            case 1:
-                System.out.println("Employee is Present");
-                hours = 8;
-                break;
-            case 2:
-                //Adding partTime
-                System.out.println("Employee is PartTime");
-                hours = 4;
-                break;
-            default:
-                System.out.println("Employee is Absent");
-                break;
+            //Solving with Switch Case Statement
+            switch (rand) {
+                case 1:
+                    System.out.println("Employee is Present");
+                    hours = 8;
+                    break;
+                case 2:
+                    //Adding partTime
+                    System.out.println("Employee is PartTime");
+                    hours = 4;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+                    break;
+            }
+
+            wage = wageperhour * hours;
+            total += wage;
+            totalhours += hours;
+            System.out.println("wage is:" + wage + "\ntotal hours" + totalhours + "\ntotal wage:" + total);
+
+
         }
-
-        wage = wageperhour * hours;
-        total += wage;
-        totalhours += hours;
-        System.out.println("wage is:" + wage + "\ntotal hours" + totalhours + "\ntotal wage:" + total);
-
-
     }
 }
