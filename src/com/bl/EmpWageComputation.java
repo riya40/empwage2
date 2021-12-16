b5f171db9e88ae3905e3b9b8bc4a0893042eb691
riya40 <priyanunna20@gmail.com>
2021-12-16 22:41:21 +0530

    Added:Calculating total hours

--- src/com/bl/EmpWageComputation.java
+++ src/com/bl/EmpWageComputation.java
@@ -1,7 +1,9 @@
 package com.bl;
 
 public class EmpWageComputation {
+    public static final int Working_Hours = 100;
     public static final int Working_Days = 20;
+
     public static void main(String args[]) {
         int wageperhour = 20, wage = 0;
         int hours = 0, totalhours = 0, total = 0;
@@ -9,33 +11,35 @@ public class EmpWageComputation {
         //Displaying The Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
 
-        //Calculating the Wage for working days
-        for (int i = 0; i < Working_Days; i++) {
-            //checking the Employee Presence
-            int rand = (int) Math.floor(Math.random() * 10) % 3;
+        //Calculating the Wage for working days and Total Working hours
+        while (totalhours < Working_Hours) {
+            //Calculating the Wage for working days
+            for (int i = 0; i < Working_Days; i++) {
+                //checking the Employee Presence
+                int rand = (int) Math.floor(Math.random() * 10) % 3;
 
-            //Solving with Switch Case Statement
-            switch (rand) {
-                case 1:
-                    System.out.println("Employee is Present");
-                    hours = 8;
-                    break;
-                case 2:
-                    //Adding partTime
-                    System.out.println("Employee is PartTime");
-                    hours = 4;
-                    break;
-                default:
-                    System.out.println("Employee is Absent");
-                    break;
+                //Solving with Switch Case Statement
+                switch (rand) {
+                    case 1:
+                        System.out.println("Employee is Present");
+                        hours = 8;
+                        break;
+                    case 2:
+                        //Adding partTime
+                        System.out.println("Employee is PartTime");
+                        hours = 4;
+                        break;
+                    default:
+                        System.out.println("Employee is Absent");
+                        break;
+                }
+
+                wage = wageperhour * hours;
+                total += wage;
+                totalhours += hours;
+
             }
-
-            wage = wageperhour * hours;
-            total += wage;
-            totalhours += hours;
             System.out.println("wage is:" + wage + "\ntotal hours" + totalhours + "\ntotal wage:" + total);
-
-
         }
     }
 }
