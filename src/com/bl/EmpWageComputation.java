2780a28de5c02a826988dc29a1bf12197e8dc7a1
riya40 <priyanunna20@gmail.com>
2021-12-16 22:14:34 +0530

    Added:CAlculation using switch statement

--- src/com/bl/EmpWageComputation.java
+++ src/com/bl/EmpWageComputation.java
@@ -2,7 +2,8 @@ package com.bl;
 
 public class EmpWageComputation {
     public static void main(String args[]){
-        int wage_per_hour=20,full_day_hour=8,parttime_hour=4;
+        int wageperhour = 20, wage = 0;
+        int hours = 0, totalhours = 0, total = 0;
 
         //Displaying The Welcome Message
         System.out.println("Welcome To Employee Wage Computation");
@@ -10,20 +11,27 @@ public class EmpWageComputation {
         //checking the Employee Presence
         int rand = (int) Math.floor(Math.random() * 10) % 3;
 
-        if(rand==0){
-            System.out.println("Employee absent");
+        //Solving with Switch Case Statement
+        switch (rand) {
+            case 1:
+                System.out.println("Employee is Present");
+                hours = 8;
+                break;
+            case 2:
+                //Adding partTime
+                System.out.println("Employee is PartTime");
+                hours = 4;
+                break;
+            default:
+                System.out.println("Employee is Absent");
+                break;
         }
-        else if(rand==1) {
-            System.out.println("employee present");
-            int wage=wage_per_hour*full_day_hour;
-            System.out.println("full time employee wgae"+wage);
-        }
-        //Adding partime wage
-        else{
-            System.out.println("employee is parttime");
-            int wage=wage_per_hour*parttime_hour;
-            System.out.println("parttime wage:"+wage);
-        }
+
+        wage = wageperhour * hours;
+        total += wage;
+        totalhours += hours;
+        System.out.println("wage is:" + wage + "\ntotal hours" + totalhours + "\ntotal wage:" + total);
+
 
     }
 }
